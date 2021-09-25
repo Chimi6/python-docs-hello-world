99b97b5f5605b759a2210ab8a1b59e71e1921e6a
Chimi6 <43803253+Chimi6@users.noreply.github.com>
2021-09-25 02:09:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,4 @@ app = Flask(__name__)
 import mysql.connector
 @app.route("/")
 def hello():
-    
-    print("ding")
-    #try:
-    #cnx = mysql.connector.connect(user='internadmin@internproject', password='Fullmetal1',
-#                                     host='internproject.mysql.database.azure.com',
-#                                     database='interns')
-      #  print("connected")
-   # except Exception as e:
-    #    print("failed to connect")
-   #     print(e)
-   # cursor = cnx.cursor()
-    
-    #query = ('select * from interns.interns')
-   # cursor.execute(query)
-   # records = cursor.fetchall()
-   # cnx.close()
-   
    return "Hello, hello!"
