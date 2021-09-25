790840f996785ed0b9430c048a7394b7e6a10f30
Caleb <calebcall10@gmail.com>
2021-09-25 00:55:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ import mysql.connector
 
 @app.route("/")
 def hello():
-    return "Hello, hello!"
+    
 
 
     print("ding")
-    try:
-    cnx = mysql.connector.connect(user='internadmin@internproject', password='Fullmetal1',
-                                    host='internproject.mysql.database.azure.com',
-                                    database='interns')
-        print("connected")
-    except Exception as e:
-        print("failed to connect")
-        print(e)
+    #try:
+    #cnx = mysql.connector.connect(user='internadmin@internproject', password='Fullmetal1',
+#                                     host='internproject.mysql.database.azure.com',
+#                                     database='interns')
+      #  print("connected")
+   # except Exception as e:
+    #    print("failed to connect")
+   #     print(e)
 
-    cursor = cnx.cursor()
+   # cursor = cnx.cursor()
     
-    query = ('select * from interns.interns')
-    cursor.execute(query)
-    records = cursor.fetchall()
-    cnx.close()
-    return records
+    #query = ('select * from interns.interns')
+   # cursor.execute(query)
+   # records = cursor.fetchall()
+   # cnx.close()
+    return "Hello, hello!"
     
    
